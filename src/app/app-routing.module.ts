489a7c35dc0c31ad65cea76bc4e72499dfefd00f
Nathan Adcock <nathan.adcock43@gmail.com>
2021-08-23 15:35:15 -0500

    feat: Created two new branches and added notes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { JobFormComponent } from './job-form/job-form.component';
 import { JobListComponent } from './job-list/job-list.component';
 
+//These routes are the locations of the Jobs list and Jobs Forms which will
+//viewable on the index page
 const routes: Routes = [
   { path: 'jobs', component: JobListComponent },
   { path: 'jobs/create', component: JobFormComponent }
